50708993def4487b8919f490db9783230d485b3a
unknown <galaxi3021@gmail.com>
2019-10-13 14:08:33 -0600

    first commit

---
+++ entorno/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\erick sanchez\desktop\2019\proyecto canica\entorno\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
